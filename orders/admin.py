aff52b86c387426ea40c429953c9ed10e4c91ddf
RafaelMaldivas <rafaelmaldivas@gmail.com>
2023-10-16 20:48:50 -0300

    section pagamento and orders

--- orders/admin.py
+++ orders/admin.py
@@ -2,8 +2,14 @@ from django.contrib import admin
 from .models import Pagamento, Order, OrderProduct
 # Register your models here.
 
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ["order_number","primeiro_nome", "ultimo_nome", "telefone", "email", "cep", "logradouro", "numero", "complemento", "bairro", "cidade", "uf", "order_total", "taxa", "is_ordered"]
+    list_filter = ["is_ordered"]
+    search_fields = ["order_number","primeiro_nome", "ultimo_nome", "telefone", "email"]
+    list_per_page = 20
+
 admin.site.register(Pagamento)
-admin.site.register(Order)
+admin.site.register(Order, OrderAdmin)
 admin.site.register(OrderProduct)
 
 
--- orders/views.py
+++ orders/views.py
@@ -2,11 +2,10 @@ from django.shortcuts import render, redirect
 from carts.models import CartItem
 from .forms import OrderForm
 import datetime
-from .models import Order
+from .models import Order, OrderProduct, Pagamento
 import mercadopago
 
 from IPython.display import Image, display
-
 
 import qrcode
 from django.http import HttpResponse
@@ -53,8 +52,8 @@ def place_order(request, total=0, quantidade=0):
             data.cidade = form.cleaned_data["cidade"]
             data.uf = form.cleaned_data["uf"]
             data.order_note = form.cleaned_data["order_note"]
-            data.order_total = grand_total
-            data.taxa = taxa
+            data.order_total = round(grand_total,2)
+            data.taxa = round(taxa,2)
             data.ip = request.META.get("REMOTE_ADDR")
             data.save()
 
@@ -86,23 +85,70 @@ def place_order(request, total=0, quantidade=0):
 
 
 def pagamento(request):
+    mt = int(datetime.date.today().strftime("%m"))
+    yr = int(datetime.date.today().strftime("%Y"))
+    dt = int(datetime.date.today().strftime("%d"))
+    d = datetime.date(dt, mt, yr)
+    current_date = d.strftime("%Y%m%d")  # 06092023
+    order_number = current_date + str(order_number.id)
+    order_number.order_number = order_number
+
+    order = Order.objects.get(user=request.user, is_ordered=False, order_number=order_number)
+
+    mt = int(datetime.date.today().strftime("%m"))
+    yr = int(datetime.date.today().strftime("%Y"))
+    d = datetime.date(mt, yr)
+    current_date = d.strftime("%Y%m%d")  # 06092023
+    pagamento_id = current_date + str(pagamento_id.id)
+    pagamento_id.pagamento_id = pagamento_id
+
+    pagamento = Pagamento(
+        user = request.user,
+        pagamento_id = pagamento_id,
+        pagamento_method = "Pix",
+        total_pago = order.order_total,
+        status = "New",  
+    )
+    pagamento.save()
+
+    order.pagamento = pagamento
+    order.is_ordered = False
+    order.save()
+
+   
+    # gera o numero da order_id      
+    cart_items = CartItem.objects.filter(user=request.user)
+
+    for item in cart_items:
+        orderproduct = OrderProduct()
+        orderproduct.order_id = order.id
+        orderproduct.pagamento = pagamento
+        orderproduct.user_id = request.user.id
+        orderproduct.produto_id = item.produto_id
+        orderproduct.quantidade = item.quantidade
+        orderproduct.variations = item.variations
+        orderproduct.produto_preco = item.produto.preco
+        orderproduct.ordered = True
+        orderproduct.save()
+    
     return render(request, "orders/pagamento.html")
+
 
 
 def gerar_qrcode(request):
     email = request.GET.get("email", None)
     amount = request.GET.get("amount", None)
-
     if email:
         sdk = mercadopago.SDK(
             "APP_USR-4427427821291868-091015-077fa150887ef28ff5a3360d3ece1747-253859971"
         )
         payment_data = {
             "transaction_amount": float(amount.replace(",", ".")),
-            "description": "Super Store",
+            "description": f"Super Store - {email}",
             "payment_method_id": "pix",
             "payer": {
-                "email": email,
+                "email": request.GET.get("email", None),
+                
             },
         }
 
@@ -113,7 +159,7 @@ def gerar_qrcode(request):
             error_correction=qrcode.constants.ERROR_CORRECT_L,
             box_size=5,
             border=4,
-        )
+        ) 
         
         # Adicione os dados ao QR code
         qr.add_data(payment["point_of_interaction"]["transaction_data"]["qr_code"])
@@ -123,7 +169,7 @@ def gerar_qrcode(request):
         img = qr.make_image(fill_color="black", back_color="white")
 
         # Salve a imagem em um objeto BytesIO
-        from io import BytesIO
+        
 
         buffer = BytesIO()
         img.save(buffer, format="PNG")
